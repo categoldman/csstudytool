03b72e1444b2f9fe6b7d5e81b4c68cb80e6871d3
Cate Goldman <ckg57015@uga.edu>
2025-03-21 16:52:03 -0400

    initial setup

---
+++ src/main/java/com/categ/csstudytool/CsStudyToolApplication.java
@@ -0,0 +1,13 @@
+package com.categ.csstudytool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsStudyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsStudyToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/categ/csstudytool/CsStudyToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.categ.csstudytool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsStudyToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
