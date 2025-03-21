1d8b6bcb288e3e2a138e084ad0ecbdd17578e61f
Cate Goldman <ckg57015@uga.edu>
2025-03-21 17:49:36 -0400

    jwt token and appconfig

---
+++ src/main/java/com/categ/csstudytool/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.categ.csstudytool.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "jd2309sfhskdj49fhhe2339487uifahiz";
+
+    public static final String JWT_HEADER = "Authorization";
+}
