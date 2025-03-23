7850dc201027550fcc0d7717459f5d35a9dcac4b
Cate Goldman <ckg57015@uga.edu>
2025-03-22 23:23:20 -0400

    user model

---
+++ src/main/java/com/categ/csstudytool/model/User.java
@@ -0,0 +1,25 @@
+package com.categ.csstudytool.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "assignee",cascade=CascadeType.ALL)
+    private List<Issue> assignedIssues = new ArrayList<>();
+}
