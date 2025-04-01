a644968f9d5940ddf598159208d422c61fcc8760
Cate Goldman <ckg57015@uga.edu>
2025-04-01 17:27:37 -0400

    project service

---
+++ src/main/java/com/categ/csstudytool/service/ProjectService.java
@@ -0,0 +1,26 @@
+package com.categ.csstudytool.service;
+
+import com.categ.csstudytool.model.Project;
+import com.categ.csstudytool.model.User;
+
+import java.util.List;
+
+public interface ProjectService {
+
+    Project createProject(Project project, User user) throws Exception;
+
+    List<Project> getProjectByTeam(User user, String category, String tag) throws Exception;
+
+    Project getProjectById(Long projectId) throws Exception;
+
+    void deleteProject(Long projectId, Long userId) throws Exception;
+
+    Project updateProject(Project updatedProject, Long id) throws Exception;
+
+    void addUserToProject(Long projectId, Long userId)throws Exception;
+
+    void removeUserToProject(Long projectId, Long userId)throws Exception;
+
+    Chat getChatByProjectId(Long projectId) throws Exception;
+
+}
