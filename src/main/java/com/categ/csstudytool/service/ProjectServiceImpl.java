b03bd98d24678909ecabb12928d436822abb7637
Cate Goldman <ckg57015@uga.edu>
2025-04-07 12:24:58 -0400

    project service impl

--- src/main/java/com/categ/csstudytool/service/ProjectServiceImpl.java
+++ src/main/java/com/categ/csstudytool/service/ProjectServiceImpl.java
@@ -8,52 +8,107 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectServiceImpl implements ProjectService{
 
-
-
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ChatService chatService;
+
     @Override
     public Project createProject(Project project, User user) throws Exception {
-        return null;
+        Project createdProject = new Project();
+        createdProject.setOwner(user);
+        createdProject.setTags(project.getTags());
+        createdProject.setName(project.getName());
+        createdProject.setCategory(project.getCategory());
+        createdProject.setDescription(project.getDescription());
+        createdProject.getTeam().add(user);
+
+        Project saved = projectRepository.save(createdProject);
+        Chat chat = new Chat();
+        chat.setProject(saved);
+
+        Chat projectChat=chatService.createChat(chat);
+        saved.setChat(projectChat);
+        return saved;
     }
 
     @Override
     public List<Project> getProjectByTeam(User user, String category, String tag) throws Exception {
-        return List.of();
+        List<Project> projects = projectRepository.findByTeamContainingOrOwner(user, user);
+        if (category != null) {
+            projects=projects.stream().filter(p -> p.getCategory().equals(category))
+                    .collect(Collectors.toList());
+        }
+        if (tag != null) {
+            projects=projects.stream().filter(p -> p.getTags().contains(tag))
+                    .collect(Collectors.toList());
+        }
+        return projects;
     }
 
     @Override
     public Project getProjectById(Long projectId) throws Exception {
-        return null;
+        Optional<Project> optionalProject = projectRepository.findById(projectId);
+        if (optionalProject.isEmpty()) {
+            throw new Exception("project not found");
+        }
+        return optionalProject.get();
     }
 
     @Override
     public void deleteProject(Long projectId, Long userId) throws Exception {
+        getProjectById(projectId);
+        projectRepository.deleteById(projectId);
 
     }
 
     @Override
     public Project updateProject(Project updatedProject, Long id) throws Exception {
-        return null;
+        Project project = getProjectById(id);
+        project.setName(updatedProject.getName());
+        project.setDescription(updatedProject.getDescription());
+        project.setTags(updatedProject.getTags());
+
+        return projectRepository.save(project);
     }
 
     @Override
     public void addUserToProject(Long projectId, Long userId) throws Exception {
+        Project project = getProjectById(projectId);
+        User user = userService.findUserById(userId);
+        if(!project.getTeam().contains(user)) {
+            project.getChat().getUsers().add(user);
+            project.getTeam().add(user);
+        }
+        projectRepository.save(project);
+
 
     }
 
     @Override
     public void removeUserToProject(Long projectId, Long userId) throws Exception {
-
+        Project project = getProjectById(projectId);
+        User user = userService.findUserById(userId);
+        if(project.getTeam().contains(user)) {
+            project.getChat().getUsers().remove(user);
+            project.getTeam().remove(user);
+        }
+        projectRepository.save(project);
     }
 
     @Override
     public Chat getChatByProjectId(Long projectId) throws Exception {
-        return null;
+        Project project = getProjectById(projectId);
+        return project.getChat();
     }
 }
