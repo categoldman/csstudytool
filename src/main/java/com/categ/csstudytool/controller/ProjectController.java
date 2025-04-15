004037c77cea55a82389fd0dd9d782ee4d32ba70
Cate Goldman <ckg57015@uga.edu>
2025-04-15 14:54:32 -0400

    project controller

---
+++ src/main/java/com/categ/csstudytool/controller/ProjectController.java
@@ -0,0 +1,37 @@
+package com.categ.csstudytool.controller;
+
+import com.categ.csstudytool.model.Project;
+import com.categ.csstudytool.model.User;
+import com.categ.csstudytool.service.ProjectService;
+import com.categ.csstudytool.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    public ResponseEntity<List<Project>>getProjects(
+            @RequestParam(required = false)String category,
+            @RequestParam(required = false)String tag,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects=projectService.getProjectByTeam(user, category, tag);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+
+
+}
